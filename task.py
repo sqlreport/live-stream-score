cb5a897b29525f19fc2212a539613965b797b4dd
Tae Lee <sqlreport@mac.com>
2023-02-09 22:13:39 +0100

    redis url

--- task.py
+++ task.py
@@ -4,8 +4,8 @@ import os
 app = celery.Celery('scalingo-sample')
 
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+app.conf.update(BROKER_URL=os.environ['live-stream-score-1973.redis.a.osc-fr1.scalingo-dbs.com'],
+                CELERY_RESULT_BACKEND=os.environ['live-stream-score-1973.redis.a.osc-fr1.scalingo-dbs.com'])
 @app.task
 def hello(name):
     return "Hello "+name

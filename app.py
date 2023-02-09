30b4d2ba1ff77e8626724e4600f3a2f44b212bc9
Tae Lee <sqlreport@mac.com>
2023-02-09 22:44:07 +0100

    timer test

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-import os
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+
+class Timer:
+    def __init__(self, current_time):
+        self.current_time = current_time
+
+    def decrement(self):
+        if self.current_time > 0:
+            self.current_time = self.current_time - 1
+        return self.current_time
+
+
+t = Timer(current_time=60)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/_timer", methods=["GET", "POST"])
+def timer():
+    new_time = t.decrement()
+    return jsonify({"result": new_time})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

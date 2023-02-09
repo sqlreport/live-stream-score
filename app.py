aa99588456cbf9e6d0de99a305190800806c0043
Tae Lee <sqlreport@mac.com>
2023-02-09 23:26:11 +0100

    timer app

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('stopwatch.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

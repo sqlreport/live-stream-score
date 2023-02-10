537d86554af43527f03cdf09bd3c0b6e05de5e47
sqlreport <sqlreport@mac.com>
2023-02-10 16:08:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ def index():
 
 @app.route('/score')
 def score():
-    remainingTimeWhenStopped = int(localStorage.getItem("remainingTimeWhenStopped"))
-    minutes = Math.floor(remainingTimeWhenStopped / 60000)
-    seconds = Math.floor((remainingTimeWhenStopped % 60000) / 1000)
-    milliseconds = Math.floor(remainingTimeWhenStopped % 1000 / 100)
-    time = minutes + ":" + seconds.toString().padStart(2, "0") + "." + milliseconds
-    return render_template('score.html', time=time)
+    return render_template('score.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
